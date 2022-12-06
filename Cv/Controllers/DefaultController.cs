6f4dd004039512aa4f58cfdb1102b715b9024f28
Ayça Aksoy <aycaaksoy2000@gmail.com>
2022-12-06 23:20:51 +0300

    repository classes created

--- Cv/Controllers/DefaultController.cs
+++ Cv/Controllers/DefaultController.cs
@@ -16,5 +16,53 @@ namespace Cv.Controllers
             var values = db.AboutMes.ToList();
             return View(values);
         }
+
+        public PartialViewResult Experience()
+        {
+            var experience = db.Experiences.ToList();
+            return PartialView(experience);
+        }
+
+        public PartialViewResult Education()
+        {
+            var edu = db.Educations.ToList();
+            return PartialView(edu);
+        }
+
+        public PartialViewResult Skills()
+        {
+            var s = db.Skills.ToList();
+            return PartialView(s);
+        }
+
+        public PartialViewResult Interests()
+        {
+            var i= db.Interests.ToList();
+            return PartialView(i);
+        }
+
+        public PartialViewResult Awards()
+        {
+            var a = db.Awards.ToList();
+            return PartialView(a);
+        }
+        [HttpGet]
+        public PartialViewResult Contact()
+        {
+            var a = db.Contacts.ToList();
+            return PartialView(a);
+        }
+        [HttpPost]
+        public PartialViewResult Contact(Contact t)
+        {
+            t.Date = DateTime.Parse(DateTime.Now.ToShortDateString());
+            db.Contacts.Add(t);
+            db.SaveChanges();
+            return PartialView();
+        }
+
+
     }
+
+
 }
